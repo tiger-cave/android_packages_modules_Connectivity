914c674ace81028d200be01db10f2fbbf6c18bbd
GuaiYiHu <guaiyihu@foxmail.com>
2026-08-11 04:25:02 +0800

    Connectivity: Resolve BpfMap isOk overload ambiguity
    Change-Id: I79d064e3a7516b64745a86b1974c328c138e4335

--- bpf/headers/include/bpf/BpfMap.h
+++ bpf/headers/include/bpf/BpfMap.h
@@ -64,7 +64,7 @@ class BpfMapRO {
     }
 
   public:
-    bool isOk(bool writable = false) const {
+    bool isOk(bool writable) const {
         if (!mMapFd.ok()) return false;
         if (isAtLeastKernelVersion(4, 14, 0)) {
             int flags = bpfGetFdMapFlags(mMapFd);
